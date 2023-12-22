260c0abad8e5dc3605aadc57c8f6a352c84924a0
Kevin N. Haw <138092261+KNHaw@users.noreply.github.com>
2023-12-21 20:28:01 -0800

    Add code baseline.

---
+++ messagePopup/popup.js
@@ -0,0 +1,216 @@
+//
+// popup.js - Code for the Obsidian Clipper AddOn for Thunderbird to save a selected mail message .
+//
+
+
+
+/* generic error handler */
+function onError(error) {
+  console.log("popup.js: " + error);
+}
+
+
+// NOTE: This code is dependent on the storage.js functions. Be sure to load them in the invoking .html page:
+//    <script src="../storage.js"></script>
+// (And, yes, there are better ways to do this. If it bugs you, let me know and I'll fix it later)
+
+// Function to change characters that are illegal in Obsidian file names to something palatable
+function correctObsidianFilename(noteFileName, useAnsiChars=true)
+{
+    if(true == useAnsiChars)
+    {
+        // Use Unicode characters to replace illegal ones
+        noteFileName = noteFileName.replaceAll('|',  '\u2223');     // Mathamatical OR operator U+2223
+        noteFileName = noteFileName.replaceAll('/',  '\u29F8');     // Big solidus U+29F8
+        noteFileName = noteFileName.replaceAll('\u005c', '\u29F9'); // Big reverse solidus U+29F9
+        noteFileName = noteFileName.replaceAll('"',  '\u201C');     // Curved opening quote U+201C
+        noteFileName = noteFileName.replaceAll('<',  '\u02C2');     // Unicode less than U+02C2
+        noteFileName = noteFileName.replaceAll('>',  '\u02C3');     // Unicode greater than U+02C2
+        noteFileName = noteFileName.replaceAll('*',  '\u2217');     // Asterisk operator U+2217 
+        noteFileName = noteFileName.replaceAll(':',  '\uA789');     // Letter colon U+A789
+        noteFileName = noteFileName.replaceAll('?',  '\u0294');     // Glottal stop U+0294
+    }
+    else
+    {
+        // Use just dash characters to replace illegal ones
+        noteFileName = noteFileName.replaceAll('|',  '-');
+        noteFileName = noteFileName.replaceAll('/',  '-');
+        noteFileName = noteFileName.replaceAll('\\', '-');
+        noteFileName = noteFileName.replaceAll('"',  '-');
+        noteFileName = noteFileName.replaceAll('<',  '-');
+        noteFileName = noteFileName.replaceAll('>',  '-');
+        noteFileName = noteFileName.replaceAll('*',  '-');
+        noteFileName = noteFileName.replaceAll(':',  '-');
+        noteFileName = noteFileName.replaceAll('?',  '-');
+    }
+    
+    return noteFileName;
+}
+
+// Function to extract text from a message object (specifically, a messagePart object),
+// then recurse through any part[] arrays beneath that for more text.
+function buildMessageBody(msgPart)
+{
+    let messageText = "";
+    
+    // See if there's plaintext email content
+    if (typeof msgPart.body !== 'undefined' && msgPart.contentType == "text/plain") {
+            messageText = messageText + msgPart.body;
+        }
+        
+    // Is there a parts[] array?
+    if(typeof msgPart.parts !== 'undefined') {
+        // Loop through all elements of the parts[] array
+        for (let i = 0; i < msgPart.parts.length; ++i) {
+            // For each of those elements, add element's .body, if it exists
+            messageText = messageText + buildMessageBody(msgPart.parts[i]);
+        }
+    }
+    return messageText;
+}
+
+// Function to actually clip the email. Pass in the saved array of parameters.
+async function clipEmail(storedParameters)
+{
+    // Read the passed parameters that configure the app.
+    
+    let obsidianVaultName = "";
+    let noteFolderPath = "";
+    let useUnicodeInFilenames = "";
+    let noteTitleTemplate = "";
+    let noteTemplate = "";
+
+    // Check stored parameters
+    if( (storedParameters["obsidianVaultName"] == undefined) ||
+        (storedParameters["noteFolderPath"] == undefined) ||
+        (storedParameters["unicodeCharSub"] == undefined) ||
+        (storedParameters["noteFilenameTemplate"] == undefined) ||
+        (storedParameters["noteContentTemplate"] == undefined) ) {
+            alert("ERROR: Please configure ObsidianClipper on its Options page before using.\n" +
+                "Look in Settings->Add-Ons Manager->Obsidian Clipper->Options tab");
+            return;
+        } else {
+        // Load parameters from storage
+        obsidianVaultName = storedParameters["obsidianVaultName"];
+        noteFolderPath = storedParameters["noteFolderPath"];
+        useUnicodeInFilenames = storedParameters["unicodeCharSub"];
+        noteTitleTemplate = storedParameters["noteFilenameTemplate"];
+        noteTemplate = storedParameters["noteContentTemplate"];
+        }
+
+    // Get the active tab in the current window using the tabs API.
+    let tabs = await messenger.tabs.query({ active: true, currentWindow: true });
+
+    // Get the message currently displayed in the active tab, using the
+    // messageDisplay API. Note: This needs the messagesRead permission.
+    // The returned message is a MessageHeader object with the most relevant
+    // information.
+    let message = await messenger.messageDisplay.getDisplayedMessage(tabs[0].id);
+
+    // Request the full message to access its full set of headers.
+    let full = await messenger.messages.getFull(message.id);
+
+    // Extract data from the message
+    let messageSubject = message.subject;
+    let messageAuthor = message.author;
+    let messageDate = message.date.toLocaleDateString();
+    let messageTime = message.date.toLocaleTimeString();
+
+    // Build comma delimited list of recipients from message
+    let messageRecipients = "";
+    if(message.recipients.length == 0) {
+        messageRecipients = "None Listed";
+    }
+    else {
+        for (let index = 0; index < message.recipients.length; ++index) {
+            // Add commas if we have a multi recipent list
+            if(index > 0) {
+                messageRecipients = messageRecipients + ", ";
+            }
+            
+            // Add next recipient
+            const nextRecipient = message.recipients[index];
+            messageRecipients = messageRecipients + nextRecipient;
+        }
+    }
+        
+
+    // Update the HTML fields of popup.html with the message subject, sender and date.
+    document.getElementById("subject").textContent = messageSubject;
+    document.getElementById("from").textContent = messageAuthor;
+    document.getElementById("received").textContent = messageDate + ", " + messageTime;
+    document.getElementById("status").textContent = "Sending data to Obsidian application.";
+
+    // Extract message body text from the message.
+    let messageBody = "";
+    messageBody = buildMessageBody(full);
+
+    // Build note name and content from templates and message data.
+    // Use these placeholders for note and time content:
+    //     Note Info: _NOTEDATE, _NOTETIME
+    //     Message info: _MSGDATE, _MSGTIME, _MSGSUBJECT, _MSGRECIPENTS, _MSGAUTHOR, _MSGCONTENT
+
+    // Create a mapping of template fields to the data to be inserted and an regular expression to use it.
+    const thisMoment = new Date();   // For note time and date
+    var templateMap = {
+        _MSGDATE:message.date.toLocaleDateString(),
+        _MSGTIME:message.date.toLocaleTimeString(),
+        _MSGSUBJECT:messageSubject,
+        _MSGRECIPENTS:messageRecipients,
+        _MSGAUTHOR:messageAuthor,
+        _MSGCONTENT:messageBody,
+        _NOTEDATE:thisMoment.toLocaleDateString(),
+        _NOTETIME:thisMoment.toLocaleTimeString(),
+    };
+    const templateRegExp = /_MSGDATE|_MSGTIME|_MSGSUBJECT|_MSGRECIPENTS|_MSGAUTHOR|_MSGCONTENT|_NOTEDATE|_NOTETIME/gi;
+
+    // Substitute the template fields with the actual message and note data
+    let noteSubject = noteTitleTemplate.replaceAll(templateRegExp, function(matched){
+      return templateMap[matched];
+    });
+    let noteContent = noteTemplate.replaceAll(templateRegExp, function(matched){
+      return templateMap[matched];
+    });
+
+    // Now, replace characters that are not supported in Obsidian filenames.
+    noteSubject = correctObsidianFilename(noteSubject, useUnicodeInFilenames);
+
+    console.log("popup.js: Note subject: " + noteSubject);
+    console.log("popup.js: Note content:\n" + noteContent);
+    
+    // Build the Obsidian URI, encoding characters like spaces or punctuation as required.
+    // Start with the vault name.
+    let obsidianUri = "obsidian://new?vault=" + obsidianVaultName;
+
+    // The PATH parameter to the URI is optional. Only add it if user specified a non-blank path.
+    if(noteFolderPath != undefined && /[^\s]/.test(noteFolderPath) ) {
+        // Path specified. Use FILE parameter to specify where the file should
+        // go and what it should be called.
+        obsidianUri = obsidianUri + "&file=" + encodeURIComponent(noteFolderPath + "/" + noteSubject);
+    } else {
+        // Path not specified. Use NAME parameter to specify file anme and that it should be placed
+        // at Obsidian's default location for notes.
+        obsidianUri = obsidianUri + "&name=" + encodeURIComponent(noteSubject);
+    }
+    
+    // Finally, append the actual email content as the note content.
+    obsidianUri = obsidianUri + "&content=" + encodeURIComponent(noteContent);
+    console.log("popup.js: obsidianUri: " + obsidianUri);
+
+
+
+    
+    // Create new note
+    // Supposedly, there's a defacto 200 char limit to URIs (https://stackoverflow.com/questions/417142/what-is-the-maximum-length-of-a-url-in-different-browsers)
+    // HOwever, testing fo 12K+ emails is fine.
+    // TODO: If there's problems with long notes, add a loop with APPEND for note content to handle bigger emails.
+    let openedWindow;
+    openedWindow = window.open(obsidianUri, "_self");
+
+    // Put up a status message in case above window open failed.
+    document.getElementById("status").textContent = "If this window does not close, something has failed. Check the Vault Name and other parameters in the Obsidian Clipper addOn options window.";
+}
+
+// Get the stored parameters and pass them to a function to populate fields.
+browser.storage.local.get(null).then(clipEmail, onError);
+
---
+++ options/options.js
@@ -0,0 +1,145 @@
+//
+// options.js - Handle Options tab for Obsidian Clipper AddOn for Thunderbird.
+//
+
+/* generic error handler */
+function onError(error) {
+  console.log("options.js: " + error);
+}
+
+
+///////////////////////////////////////////////////////////////
+// DEBUG: Start with a clean slate when tesing AddOn...
+// console.log("DEBUG: Clearing local store values for testing...");
+// browser.storage.local.clear();
+///////////////////////////////////////////////////////////////
+
+// Set up array of default parameters for each HTML field.
+// remember to assign listeners to any new field (below)
+var defaultParameters = [];
+defaultParameters["obsidianVaultName"] = "_myNotes";
+defaultParameters["noteFolderPath"] = "";
+defaultParameters["unicodeCharSub"] = true;
+defaultParameters["noteFilenameTemplate"] = "Email (_MSGDATE) : _MSGSUBJECT";
+defaultParameters["noteContentTemplate"] = 
+    "Tagged: #email\n" +
+    "Created: _NOTEDATE, _NOTETIME\n" +
+    "Subject: _MSGSUBJECT\n" +
+    "Message Date: _MSGDATE, _MSGTIME\n" +
+    "Author: _MSGAUTHOR\n" +
+    "Recipients: _MSGRECIPENTS\n\n" +
+    "---\n\n" +
+    "_MSGCONTENT";
+
+
+
+// Store the data to local storage with the given key
+function parameterStore(key, value) {
+    storeLocal = browser.storage.local.set({ [key] : value });
+    storeLocal.then(() => {
+        console.log("parameterStore: Stored parameter [" + key + ", " + value + "] success");  // Huh? Not seeing this on console, but appears to work.
+      }, onError);
+}
+
+// Store the contents of an options field to local storage
+function storeOption(id) {
+    fieldContent = "";
+    
+    // Read the options field
+    var elem = document.getElementById(id);
+    if(typeof elem !== 'undefined' && elem !== null) {
+        
+        if(elem.type == "checkbox") {
+            // Unlike text fields, read boolean to see if checkboxes are set or cleared
+            fieldContent = elem.checked;
+        } else {
+            // Read field
+            fieldContent = elem.value;
+        }
+        
+        // Store away field data
+        parameterStore(id, fieldContent);
+    }
+    else {
+        console.log("storeOption("+id+") ERROR: typeof elem == " + typeof elem + "elem == " + elem);
+    }
+}
+
+
+// Store the default value of an option to local storage
+function storeDefault(id) {
+    defaultValue = defaultParameters[id];
+    
+    // Is element in the array of default values?
+    if(undefined != defaultValue) {
+        // There is an entry - save this default value away.
+        console.log("storeDefault("+id+") storing default value of "+defaultValue);
+        parameterStore(id, defaultValue);
+    } else {
+        console.log("ERROR: storeDefault("+id+") can't find a default value");
+    }
+}
+
+function loadOptionsFields(storedParameters)
+{
+    // Loop through list of expected parameters to set the fields
+    for(key in defaultParameters) {
+        fieldContent = "";
+        if(storedParameters[key] == undefined) {
+            console.log("loadOptionsFields: Parameter ["+key+"] not found. Using default value \"" + defaultParameters[key] +"\'");
+            
+            // Save field content
+            fieldContent =  defaultParameters[key];  // Save filed content
+            
+            // Store default parameter
+            parameterStore(key, fieldContent);
+            
+        } else  {
+            console.log("loadOptionsFields: Parameter ["+key+"] found. Using value \"" + storedParameters[key] +"\'");
+            
+            // Save field content
+            fieldContent =  storedParameters[key];  
+        }
+        
+        // Now set the field's value on the options webpage.
+        var elem = document.getElementById(key);
+        if(typeof elem !== 'undefined' && elem !== null) {
+            if(elem.type == "checkbox") {
+                // Unlike text fields, use a boolean to set/clear checkboxes
+                elem.checked = fieldContent;
+            } else {
+                // Set field to the indicated string
+                elem.value = fieldContent;
+            }
+        }
+    }
+}
+
+///////////////////////
+// Main execution path
+///////////////////////
+
+// Set up event listeners for buttons
+document.getElementById('submit-obsidianVaultName').onclick = function() {storeOption("obsidianVaultName"); };
+document.getElementById('default-obsidianVaultName').onclick = function() {storeDefault("obsidianVaultName"); };
+
+document.getElementById('submit-noteFolderPath').onclick = function() {storeOption("noteFolderPath"); };
+document.getElementById('default-noteFolderPath').onclick = function() {storeDefault("noteFolderPath"); };
+
+document.getElementById('submit-unicodeCharSub').onclick = function() {storeOption("unicodeCharSub"); };
+document.getElementById('default-unicodeCharSub').onclick = function() {storeDefault("unicodeCharSub"); };
+
+document.getElementById('submit-noteFilenameTemplate').onclick = function() {storeOption("noteFilenameTemplate"); };
+document.getElementById('default-noteFilenameTemplate').onclick = function() {storeDefault("noteFilenameTemplate"); };
+
+document.getElementById('submit-noteContentTemplate').onclick = function() {storeOption("noteContentTemplate"); };
+document.getElementById('default-noteContentTemplate').onclick = function() {storeDefault("noteContentTemplate"); };
+
+
+
+
+
+// Get the stored parameters and pass them to a function to populate fields.
+browser.storage.local.get(null).then(loadOptionsFields, onError);
+
+
